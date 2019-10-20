a7aa993b89760de8bd893cc6cb0f21626184eee7
Daniel Huisman <daniel@huisman.me>
2019-10-20 17:17:08 +0200

    Add more patches for all drivers

--- drivers/media/tuners/av201x.c
+++ drivers/media/tuners/av201x.c
@@ -259,8 +259,8 @@ static int av201x_get_rf_strength(struct dvb_frontend *fe, u16 *st)
 static const struct dvb_tuner_ops av201x_tuner_ops = {
 	.info = {
 		.name           = "Airoha Technology AV201x",
-		.frequency_min_hz = 850 * MHz,
-		.frequency_max_hz = 2300 * MHz,
+		.frequency_min = 850000000,
+		.frequency_max = 2300000000,
 	},
 
 	.release = av201x_release,
--- drivers/media/tuners/stv6120.c
+++ drivers/media/tuners/stv6120.c
@@ -539,8 +539,8 @@ static int get_rf_strength(struct dvb_frontend *fe, u16 *agc)
 static struct dvb_tuner_ops tuner_ops = {
 	.info = {
 		.name = "STV6120",
-		.frequency_min_hz  =  250 * MHz,
-		.frequency_max_hz  = 2300 * MHz,
+		.frequency_min  =  250000000,
+		.frequency_max  = 2300000000,
 	},
 	.init              = init,
 	.sleep             = sleep,
